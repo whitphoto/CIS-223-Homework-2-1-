86a04daaf10678795ec386f278b75530c50e8521
whitphoto <nate@whitchurchphotography.com>
2018-03-30 20:04:32 -0400

    A Calculator app

---
+++ Lab2 Calculator/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab2_Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+         
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x1 = int.Parse(TextBox1.Text);
+            int x2 = int.Parse(TextBox2.Text);
+            int x3;
+            x3 = x1 + x2;
+            string answer = x3.ToString();
+            TextBox3.Text = answer;
+
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "0";
+            TextBox2.Text = "0";
+            TextBox3.Text = "0";
+
+
+        }
+
+        private void MultButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x1 = int.Parse(TextBox1.Text);
+            int x2 = int.Parse(TextBox2.Text);
+            int x3;
+            x3 = x1 * x2;
+            string answer = x3.ToString();
+            TextBox3.Text = answer;
+
+        }
+    }
+}
---
+++ Lab2 Calculator/ReadMe.md
@@ -0,0 +1,16 @@
+/********************************************************
+*														*
+*		Nathan Whitchurch								*
+*		CIS 223 CIS223: COMPUTER PROJECTS & APPLICATNS	*
+*		Dr Whittle										*
+*		Homework 2-1 Calculator 						*
+*														*
+********************************************************/
+
+Instructions
+1.	Complete a Calculator program. The program should allow
+ the user to enter numbers into two textboxes on the first 
+ page.  Two buttons should be available: a button to add the
+ numbers and a button to multiply the numbers.  When pressed,
+ the sum or product should be displayed in a third textbox.  
+ A third button should be used to set all the textboxes to zero.     
